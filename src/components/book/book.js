7540fd763749b0d322c96b015cb98c18fd6f67da
JesseLiii <jesseli751@gmail.com>
2022-09-03 16:17:56 -0400

    working book css

---
+++ src/components/book/book.js
@@ -0,0 +1,40 @@
+import React, { Component, useState } from 'react';
+import './book.css';
+
+const Book = () => {
+	return (
+		<div class='bg'>
+			<div class='book1'>
+				{/* <div class='ribbon1'></div> */}
+				<div class='bookCut'></div>
+			</div>
+			<div class='book2'>
+				{/* <div class='ribbonCut1'></div> */}
+				{/* <div class='ribbonCut2'></div> */}
+				{/* <div class='ribbonCut3'></div> */}
+			</div>
+
+			<div class='book3'>
+				{/* <div class='ribbon3'></div> */}
+				<div class='bookCut3'></div>
+			</div>
+			<div class='book4'>
+				{/* <div class='ribbonCut4'></div> */}
+				{/* <div class='ribbonCut5'></div> */}
+				{/* <div class='ribbonCut6'></div> */}
+			</div>
+			<div class='book5'></div>
+			<div class='book6'>
+				{/* <div class='ribbon6'></div> */}
+				<div class='bookCut6'></div>
+			</div>
+			<div class='book7'>
+				{/* <div class='ribbonCut7'></div> */}
+				{/* <div class='ribbonCut8'></div> */}
+				{/* <div class='ribbonCut9'></div> */}
+			</div>
+		</div>
+	);
+};
+
+export default Book;
